73af1e92b46122de95c91f75976a6bca6425da04
Fogrex <fogrexon@gmail.com>
2022-11-06 13:05:49 +0900

    initial

--- chapter5/src/stage.ts
+++ chapter5/src/stage.ts
@@ -1,13 +1,11 @@
 import {
   BoxGeometry,
   Camera,
-  ConeGeometry,
   EventDispatcher,
   Mesh,
   MeshBasicMaterial,
   Renderer,
   Scene,
-  SphereGeometry,
 } from 'three';
 import {
   BoardBit, judgeResult, Player, toBit,
@@ -23,10 +21,6 @@ export class Stage extends EventDispatcher {
 
   private crossModelTemplate: Mesh | null = null;
 
-  private circleBit: BoardBit = 0;
-
-  private crossBit: BoardBit = 0;
-
   constructor(renderer: Renderer, scene: Scene, camera: Camera) {
     super();
 
@@ -37,57 +31,30 @@ export class Stage extends EventDispatcher {
     // this.circleModelTemplate =
     // this.crossModelTemplate =
 
-    const board = new Mesh(
-      new BoxGeometry(9, 0.1, 9),
-      new MeshBasicMaterial({ color: 0xdddddd }),
-    );
-    board.position.set(0, 0.05, 0);
-    scene.add(board);
+    // 1章 ボードを用意する
 
-    const wall = new Mesh(
-      new BoxGeometry(9, 0.4, 0.1),
-      new MeshBasicMaterial({ color: 0x333333 }),
-    );
-    wall.position.set(0, 0.2, 1.5);
-    board.add(wall);
+    // const board =
+    // const wall1 =
+    // const wall2 =
+    // const wall3 =
+    // const wall4 =
 
-    const wall2 = new Mesh(
-      new BoxGeometry(9, 0.4, 0.1),
-      new MeshBasicMaterial({ color: 0x333333 }),
-    );
-    wall2.position.set(0, 0.2, -1.5);
-    board.add(wall2);
+    // クリック判定 1章を組んだのちにコメントアウトを外す
+    // this.interaction = new Interaction(renderer, board, camera);
+    // this.interaction.addEventListener('click', (event) => {
+    //   if (event.intersects.length === 0) return;
+    //   if (judgeResult(this.circleBit, this.crossBit) !== 'none') return;
+    //   const intersect = event.intersects[0];
+    //   const { point } = intersect;
+    //   const x = Math.round(point.x / 3);
+    //   const y = Math.round(point.z / 3);
 
-    const wall3 = new Mesh(
-      new BoxGeometry(0.1, 0.4, 9),
-      new MeshBasicMaterial({ color: 0x333333 }),
-    );
-    wall3.position.set(1.5, 0.2, 0.0);
-    board.add(wall3);
+    //   const bit = toBit(x, y);
 
-    const wall4 = new Mesh(
-      new BoxGeometry(0.1, 0.4, 9),
-      new MeshBasicMaterial({ color: 0x333333 }),
-    );
-    wall4.position.set(-1.5, 0.2, 0.0);
-    board.add(wall4);
-
-    // クリック判定
-    this.interaction = new Interaction(renderer, board, camera);
-    this.interaction.addEventListener('click', (event) => {
-      if (event.intersects.length === 0) return;
-      if (judgeResult(this.circleBit, this.crossBit) !== 'none') return;
-      const intersect = event.intersects[0];
-      const { point } = intersect;
-      const x = Math.round(point.x / 3);
-      const y = Math.round(point.z / 3);
-
-      const bit = toBit(x, y);
-
-      // eslint-disable-next-line no-bitwise
-      if ((this.circleBit & bit) !== 0 || (this.crossBit & bit) !== 0) return;
-      this.dispatchEvent({ type: 'place', x, y });
-    });
+    //   // eslint-disable-next-line no-bitwise
+    //   if ((this.circleBit & bit) !== 0 || (this.crossBit & bit) !== 0) return;
+    //   this.dispatchEvent({ type: 'place', x, y });
+    // });
   }
 
   // マーカーを配置する
