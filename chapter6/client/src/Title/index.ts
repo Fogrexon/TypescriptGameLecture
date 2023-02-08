b21dea653d16e069876f4e5c497af34bdacc90cc
Fogrex <fogrexon@gmail.com>
2023-02-08 20:33:17 +0900

    タイトル画面

--- chapter6/client/src/Title/index.ts
+++ chapter6/client/src/Title/index.ts
@@ -1,30 +1,46 @@
 import { Network } from "../Network";
-import { IScreenControllable } from "../Screen/ScreenManager";
+import { IScreenControllable, ScreenManager } from "../Screen/ScreenManager";
 import { globalState } from "../globalState";
 
 export class Title implements IScreenControllable {
 
+  // dom関連
   private parent: HTMLElement;
+  private nameField: HTMLInputElement;
+  private joinButton: HTMLButtonElement;
+  
+  // 通信関連
+  private network: Network;
+
   constructor(parent: HTMLElement, network: Network) {
     this.parent = parent;
+    this.nameField = this.parent.querySelector('#name') as HTMLInputElement;
+    this.joinButton = this.parent.querySelector('#join') as HTMLButtonElement;
+    this.network = network;
   }
 
-  private initialize() {
-    const nameField = this.parent.querySelector('#name') as HTMLInputElement;
-    const joinButton = this.parent.querySelector('#join') as HTMLButtonElement;
-
-    nameField.addEventListener('change', (event) => {
-      const target = event.target as HTMLInputElement;
-      // 本来ならここに名前のバリデーションを入れる
-      globalState.playerName = target.value;
-    })
-  }
-
-
+  /**
+   * タイトル画面開始
+   */
   public startScreen(): void {
-    console.log('start title')
+    this.parent.dataset.visible = 'true'
+    this.joinButton.addEventListener('click', this.joinButtonHandler.bind(this));
   }
+  /**
+   * タイトル画面終了
+   * ゲーム画面へ移動
+   */
   public endScreen(): void {
-    console.log('end title')
+    this.parent.dataset.visible = 'false'
+    this.joinButton.removeEventListener('click', this.joinButtonHandler.bind(this));
+  }
+
+  /**
+   * 参加ボタンが押されたときの処理
+   */
+  private joinButtonHandler(): void {
+    this.network.sendJoinRoom(this.nameField.value);
+    globalState.playerName = this.nameField.value;
+    ScreenManager.moveScreen('game');
   }
 }
